dfdc1ec1b87c74e3dd1a845d441da204d6e7f932
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-20 11:34:21 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,38 @@
 #include <cstdio>
 #include <ctime>
 
-void insertionSort(std::vector<int> algo) {
+void insertionSort(std::vector<int> arr) {
     for (int i = 1; i < algo.size(); i++) {
-        int key = algo[i];
+        int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && algo[j] > key) {
-            algo[j + 1] = algo[j];
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
             j = j - 1;
         }
-        algo[j + 1] = key;
+        arr[j + 1] = key;
+    }
+}
+
+
+int partition(std::vector<int> arr, int minIndex, int maxIndex){
+    int pivot = arr[maxIndex];
+    int i = (minIndex - 1);
+
+    for (int j = minIndex; j <= maxIndex - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[maxIndex]);
+    return (i + 1);
+}
+
+void quickSort(std::vector<int> arr, int minIndex, int maxIndex){
+    if (minIndex < maxIndex) {
+        int partIndex = partition(arr, minIndex, maxIndex);
+        quickSort(arr, minIndex, partIndex - 1);
+        quickSort(arr, partIndex + 1, maxIndex);
     }
 }
 
