3eaca332bd7d31e6a5a44ae268e189881265a155
jonathanok1 <113383828+jonathanok1@users.noreply.github.com>
2023-07-22 17:52:56 -0400

    Update algo.cpp
    split into three files

--- algo.cpp
+++ algo.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <vector>
+#include "algo.h"
+#include <iterator>
 
+bool SortAlgo::check (int i,int j) {
+    return (i < j);
+}
+
+void SortAlgo::insertionSort(std::vector<int>& dataSet) {
+    for (int i = 1; i < dataSet.size(); i++) {
+        int key = dataSet[i];
+        int j = i - 1;
+        while (j >= 0 && dataSet[j] > key) {
+            dataSet[j + 1] = dataSet[j];
+            j = j - 1;
+        }
+        dataSet[j + 1] = key;
+    }
+}
+
+void SortAlgo::printAndInsertionSort(std::string description, std::vector<int>& dataSet) {
+    std::cout << "Current " << description << " List" << std::endl;
+    std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::cout << "After Insertion Sort" << std::endl;
+    insertionSort(dataSet);
+    std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n" << std::endl;
+}
+
+int SortAlgo::partition(std::vector<int> arr, int minIndex, int maxIndex){
+    int pivot = arr[maxIndex];
+    int i = (minIndex - 1);
+
+    for (int j = minIndex; j <= maxIndex - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[maxIndex]);
+    return (i + 1);
+}
+
+void SortAlgo::quickSort(std::vector<int> arr, int minIndex, int maxIndex){
+    if (minIndex < maxIndex) {
+        int partIndex = partition(arr, minIndex, maxIndex);
+        quickSort(arr, minIndex, partIndex - 1);
+        quickSort(arr, partIndex + 1, maxIndex);
+    }
+}
