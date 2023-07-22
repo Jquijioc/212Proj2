8ecbace317264d915a4ba18d5530d33623e84c38
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-22 18:37:02 -0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -2,10 +2,20 @@
 #include <vector>
 
 class Sort {
+    private:
+        int size;
+        std::vector<int> sorted;
+        std::vector<int> reversed;
+        std::vector<int> random;
+        std::vector<int> partial;
     public:
-    void insertionSort(std::vector<int>& arr);
-    void printAndInsertionSort(std::string description, std::vector<int>& dataSet);
-    void quickSort(std::vector<int> arr, int minIndex, int maxIndex);
-    static bool check (int i, int j);
-    int partition(std::vector<int> arr, int minIndex, int maxIndex);
+        Sort();
+        Sort(int size, std::vector<int> sorted, std::vector<int> reversed, std::vector<int> random, std::vector<int> partial);
+        void benchmark();
+        void printSort(int sor);
+        static bool check (int i, int j);
+        void insertionSort(std::vector<int>& arr);
+        void printAndInsertionSort(std::string description, std::vector<int>& dataSet);
+        void quickSort(std::vector<int> arr, int minIndex, int maxIndex);
+        int partition(std::vector<int> arr, int minIndex, int maxIndex);
 };
