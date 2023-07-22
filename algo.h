03999ad3a01fc7767606da39261e59f89af1293d
jonathanok1 <113383828+jonathanok1@users.noreply.github.com>
2023-07-22 17:51:09 -0400

    Update algo.h
    header file for sorting algorithms

--- algo.h
+++ algo.h
@@ -1,1 +1,8 @@
-
+class SortAlgo {
+    public:
+    void insertionSort(std::vector<int>& arr);
+    void printAndInsertionSort(std::string description, std::vector<int>& dataSet);
+    void quickSort(std::vector<int> arr, int minIndex, int maxIndex);
+    static bool check (int i, int j);
+    int partition(std::vector<int> arr, int minIndex, int maxIndex);
+};
