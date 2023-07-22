101917869a7ba02cf08df05f6ca1631216aa0a8d
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-22 19:06:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@
 #include <fstream>
 #include <sstream>
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
     std::string file_name(argv[1]); //Get file name.
 
     std::vector<int> file_data;
     std::ifstream file(file_name);
 
-    std::vector<std::vector<int>> sequence;
+    std::vector <std::vector<int>> sequence;
     std::string line;
 
-    while (std::getline(file, line)){
+    while (std::getline(file, line)) {
         int i = 0;
         std::stringstream str(line);
         std::vector<int> seq;
@@ -31,4 +31,31 @@ int main(int argc, char* argv[]){
     }
 
     Sort sort(sequence[0].size(), sequence[0], sequence[1], sequence[2], sequence[3]);
+
+    std::string input;
+
+    std::cout << "Input an integer to sort via algorithm:" << std::endl;
+    std::cout << "1: Insertion Sort" << std::endl;
+    std::cout << "2: Quick Sort" << std::endl;
+    std::cout << "3: Merge Sort" << std::endl;
+    std::cout << "4: Cycle Sort" << std::endl;
+    std::cout << "5: Quit" << std::endl;
+
+    while(getline(std::cin, input) && input != "5"){
+        if(input == "5"){
+            break;
+        } else if (input == "1"){
+            std::cout << "INSERTION SORT HERE" << std::endl;
+        } else if (input == "2"){
+            std::cout << "QUICK SORT HERE" << std::endl;
+        } else if (input == "3"){
+            std::cout << "MERGE SORT HERE" << std::endl;
+        } else if(input == "4"){
+            std::cout << "CYCLE SORT HERE" << std::endl;
+        } else {
+            std::cout << "Unrecognized command: " << input << std::endl;
+        }
+    }
+
+    return 0;
 }
