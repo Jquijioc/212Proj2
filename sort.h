b6a2f1eb5a0bd80b1aadb92bfcbd4643343b27e9
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-22 20:13:50 -0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -11,13 +11,12 @@ class Sort {
     public:
         Sort();
         Sort(int size, std::vector<int> sorted, std::vector<int> reversed, std::vector<int> random, std::vector<int> partial);
-        void benchmark();
-        void print(int alg);
+        void benchmark(int alg);
+        void print(int alg, std::vector<int> dataSet);
         void printSort(int alg);
-        static bool check (int i, int j);
         std::vector<int> insertionSort(std::vector<int> arr);
+        std::vector<int> mergeSort(std::vector<int> arr);
         void cycleSort(std::vector<int>& arr);
-        void printAndInsertionSort(std::string description, std::vector<int> dataSet);
-        void quickSort(std::vector<int> arr, int minIndex, int maxIndex);
-        int partition(std::vector<int> arr, int minIndex, int maxIndex);
+        void quickSort(std::vector<int>& arr, int minIndex, int maxIndex);
+        int partition(std::vector<int>& arr, int minIndex, int maxIndex);
 };
