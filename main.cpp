867c6923af1e1f144484e58fa33d86a067d22f3d
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-23 17:46:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,9 @@
 #include "sort.h"
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <sstream>
 
-int main(int argc, char* argv[]) {
-    std::string file_name(argv[1]); //Get file name.
-
-    std::ifstream file(file_name);
-
-    std::vector <std::vector<KeyedInt>> sequence;
-    std::string line;
-
-    while (std::getline(file, line)) {
-        std::stringstream str(line);
-        std::vector<KeyedInt> seq;
-
-        //The dec means declaration--it labels sorted, reversed, randomly sorted, and partially sorted datasets. We drop it to only account for the input file.
-        std::string dec;
-        str >> dec;
-
-        int num;
-        char key = 'a';
-
-        while (str >> num) {
-            KeyedInt temp = {num, std::string(1, key)};
-            seq.push_back(temp);
-            key++;
-            }
-
-        sequence.push_back(seq);
-    }
-
-    Sort sort(sequence[0].size(), sequence[0], sequence[1], sequence[2], sequence[3]);
-    sort.mainMenu(file_name);
+int main() {
+    Sort sort;
+    sort.mainMenu();
 
     return 0;
 }
