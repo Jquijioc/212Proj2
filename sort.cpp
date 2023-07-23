614bc3288e7c22892631666759d2f9ab6fdc8294
jonathanok1 <113383828+jonathanok1@users.noreply.github.com>
2023-07-22 20:20:59 -0400

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -121,6 +121,67 @@ void Sort::quickSort(std::vector<int>& arr, int minIndex, int maxIndex){
     }
 }
 
+void Sort::merge(std::vector<int>& arr, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> L(n1), R(n2);
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void Sort::mergeSort(std::vector<int>& arr, int left, int right){
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void Sort::printAndMergeSort(std::string description, std::vector<int>& dataSet) {
+    std::cout << "Current " << description << " List" << std::endl;
+    std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::cout << "After Merge Sort" << std::endl;
+    mergeSort(dataSet, 0, dataSet.size() - 1);
+    std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n" << std::endl;
+}
+
 void Sort::print(int alg, std::vector<int> dataSet) {
     std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
     std::cout << std::endl;
