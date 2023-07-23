ce1e60a54f4ad9646dde8e3f7641a7b15e24f4cd
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-23 11:11:26 -0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -9,24 +9,30 @@ struct KeyedInt {
 
 class Sort {
     private:
-        int size;
         std::vector<KeyedInt> sorted;
         std::vector<KeyedInt> reversed;
         std::vector<KeyedInt> random;
         std::vector<KeyedInt> partial;
+        int size;
     public:
         //Sort constructors.
         Sort();
         Sort(int size, std::vector<KeyedInt> sorted, std::vector<KeyedInt> reversed, std::vector<KeyedInt> random, std::vector<KeyedInt> partial);
         //Helper methods
         void benchmark(int alg);
-        void print(int alg, std::vector<KeyedInt> dataSet);
+        void print(int alg, std::vector<KeyedInt> dataSet, bool displaySteps);
+        void printArray(const std::vector<KeyedInt>& arr);
         void printSort(int alg);
-        void menu(std::string file_name);
+        //Menu methods.
+        void mainMenu(std::string file_name);
+        void displaySubMenu(std::string file_name);
+        void displaySortMenu(std::string file_name);
+        void displaySort(int alg, int sort_type);
         //Sorting methods.
-        std::vector<KeyedInt> insertionSort(std::vector<KeyedInt> arr);
-        std::vector<KeyedInt> mergeSort(std::vector<KeyedInt> arr);
-        std::vector<KeyedInt> cycleSort(std::vector<KeyedInt> arr);
-        void quickSort(std::vector<KeyedInt>& arr, int minIndex, int maxIndex);
-        int partition(std::vector<KeyedInt>& arr, int minIndex, int maxIndex);
-};
+        std::vector<KeyedInt> insertionSort(std::vector<KeyedInt> arr, bool displaySteps = false);
+        void quickSort(std::vector<KeyedInt>& arr, int minIndex, int maxIndex, bool displaySteps = false);
+        int partition(std::vector<KeyedInt>& arr, int low, int high, bool displaySteps);
+        std::vector<KeyedInt> mergeSort(std::vector<KeyedInt> arr, bool displaySteps = false);
+        std::vector<KeyedInt> merge(std::vector<KeyedInt> left, std::vector<KeyedInt> right, bool displaySteps);
+        std::vector<KeyedInt> cycleSort(std::vector<KeyedInt> arr, bool displaySteps = false);
+    };
