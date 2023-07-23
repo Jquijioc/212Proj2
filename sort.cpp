3da4de5554c6cf34c7d91659b5886d38a2562432
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-22 20:13:36 -0400

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -8,6 +8,7 @@
 #include <iterator>
 #include <fstream>
 #include <sstream>
+#include <chrono>
 
 Sort::Sort(){
     int size = 0;
@@ -25,39 +26,6 @@ Sort::Sort(int size, std::vector<int> sorted, std::vector<int> reversed, std::ve
     this->partial = partial;
 }
 
-bool Sort::check (int i, int j) {
-    return (i < j);
-}
-
-void Sort::benchmark() {
-    std::clock_t start;
-    double duration;
-
-    start = std::clock();
-    printAndInsertionSort("Sorted", sorted);
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "Time to sort sorted vector: " << duration << " seconds" << std::endl;
-
-
-    start = std::clock();
-    printAndInsertionSort("Reversed", reversed);
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "Time to sort reversed vector: " << duration << " seconds" << std::endl;
-
-
-    start = std::clock();
-    printAndInsertionSort("Random", random);
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "Time to sort random vector: " << duration << " seconds" << std::endl;
-
-
-    start = std::clock();
-    printAndInsertionSort("Partial", partial);
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "Time to sort partially sorted vector: " << duration << " seconds" << std::endl;
-
-}
-
 std::vector<int> Sort::insertionSort(std::vector<int> dataSet) {
     for (int i = 1; i < dataSet.size(); i++) {
         int key = dataSet[i];
@@ -71,18 +39,67 @@ std::vector<int> Sort::insertionSort(std::vector<int> dataSet) {
     return dataSet;
 }
 
-void Sort::printAndInsertionSort(std::string description, std::vector<int> dataSet) {
-    std::cout << "Current " << description << " List" << std::endl;
-    std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
-
-    std::cout << "After Insertion Sort" << std::endl;
-    std::vector<int> sorted = insertionSort(dataSet);
-    std::copy(sorted.begin(), sorted.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << "\n" << std::endl;
+std::vector<int> Sort::mergeSort(std::vector<int> arr) {
+    if(arr.size() <= 1) {
+        return arr;
+    }
+    std::vector<int> left(arr.begin(), arr.begin() + arr.size()/2);
+    std::vector<int> right(arr.begin() + arr.size()/2, arr.end());
+    left = mergeSort(left);
+    right = mergeSort(right);
+    arr.clear();
+    while(left.size() > 0 && right.size() > 0) {
+        if(left[0] <= right[0]) {
+            arr.push_back(left[0]);
+            left.erase(left.begin());
+        } else {
+            arr.push_back(right[0]);
+            right.erase(right.begin());
+        }
+    }
+    while(left.size() > 0) {
+        arr.push_back(left[0]);
+        left.erase(left.begin());
+    }
+    while(right.size() > 0) {
+        arr.push_back(right[0]);
+        right.erase(right.begin());
+    }
+    return arr;
 }
 
-int Sort::partition(std::vector<int> arr, int minIndex, int maxIndex){
+void Sort::cycleSort(std::vector<int>& arr) {
+    for(int cycleStart = 0; cycleStart < arr.size() - 1; ++cycleStart) {
+        int item = arr[cycleStart];
+        int pos = cycleStart;
+        for(int i = cycleStart + 1; i < arr.size(); ++i) {
+            if(arr[i] < item) {
+                ++pos;
+            }
+        }
+        if(pos == cycleStart) {
+            continue;
+        }
+        while(item == arr[pos]) {
+            ++pos;
+        }
+        std::swap(item, arr[pos]);
+        while(pos != cycleStart) {
+            pos = cycleStart;
+            for(int i = cycleStart + 1; i < arr.size(); ++i) {
+                if(arr[i] < item) {
+                    ++pos;
+                }
+            }
+            while(item == arr[pos]) {
+                ++pos;
+            }
+            std::swap(item, arr[pos]);
+        }
+    }
+}
+
+int Sort::partition(std::vector<int>& arr, int minIndex, int maxIndex){
     int pivot = arr[maxIndex];
     int i = (minIndex - 1);
 
@@ -96,86 +113,85 @@ int Sort::partition(std::vector<int> arr, int minIndex, int maxIndex){
     return (i + 1);
 }
 
-void Sort::quickSort(std::vector<int> arr, int minIndex, int maxIndex){
+void Sort::quickSort(std::vector<int>& arr, int minIndex, int maxIndex){
     if (minIndex < maxIndex) {
         int partIndex = partition(arr, minIndex, maxIndex);
         quickSort(arr, minIndex, partIndex - 1);
         quickSort(arr, partIndex + 1, maxIndex);
     }
 }
-void sort::cycleSort(std::vector<int>& arr) {
-    int n = arr.size();
-    
-    for (int cycleStart = 0; cycleStart < n - 1; ++cycleStart) {
-        int item = arr[cycleStart];
-        int pos = cycleStart;
-        
-        // Find the position where we put the element 'item'
-        for (int i = cycleStart + 1; i < n; ++i) {
-            if (arr[i] < item) {
-                ++pos;
-            }
-        }
-        
-        // If the item is already in the correct position, skip this cycle
-        if (pos == cycleStart) {
-            continue;
-        }
-        
-        // Otherwise, put the item to its correct position
-        while (item == arr[pos]) {
-            ++pos;
-        }
-        std::swap(item, arr[pos]);
-        
-        // Rotate the rest of the cycle
-        while (pos != cycleStart) {
-            pos = cycleStart;
-            for (int i = cycleStart + 1; i < n; ++i) {
-                if (arr[i] < item) {
-                    ++pos;
-                }
-            }
-            while (item == arr[pos]) {
-                ++pos;
-            }
-            std::swap(item, arr[pos]);
-        }
-    }
-}
 
-void Sort::print(int alg){
-    for (int i = 0; i < this->size; i++){
-        std::cout << this->sorted[i] << ' ';
-    }
-    for (int i = 0; i < this->size; i++){
-        std::cout << this->reversed[i] << ' ';
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < this->size; i++){
-        std::cout << this->random[i] << ' ';
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < this->size; i++){
-        std::cout << this->partial[i] << ' ';
-    }
+void Sort::print(int alg, std::vector<int> dataSet) {
+    std::copy(dataSet.begin(), dataSet.end(), std::ostream_iterator<int>(std::cout, " "));
     std::cout << std::endl;
 }
 
 void Sort::printSort(int alg){
-    switch(alg) {
-        case 1:
-            print(alg);
-            break;
-        case 2:
-            print(alg);
-            break;
-        case 3:
-            print(alg);
-            break;
-        case 4:
-            print(alg);
-            break;
+    std::vector<std::vector<int>> dataSets = {this->sorted, this->reversed, this->random, this->partial};
+    std::vector<std::string> descriptions = {"Sorted", "Reversed", "Random", "Partial"};
+
+    for(int i = 0; i < dataSets.size(); i++){
+        std::vector<int> dataSet = dataSets[i];
+        std::string description = descriptions[i];
+        std::cout << "Before Sort of " << description << " List" << std::endl;
+        print(alg, dataSet);
+        switch(alg) {
+            case 1:
+                dataSet = insertionSort(dataSet);
+                break;
+            case 2:
+                quickSort(dataSet, 0, dataSet.size()-1);
+                break;
+            case 3:
+                dataSet = mergeSort(dataSet);
+                break;
+            case 4:
+                cycleSort(dataSet);
+                break;
+        }
+        std::cout << "After Sort of " << description << " List" << std::endl;
+        print(alg, dataSet);
     }
-    std::cout << std::endl;
+}
+
+void Sort::benchmark(int alg) {
+    std::vector<std::vector<int>> dataSets = {this->sorted, this->reversed, this->random, this->partial};
+    std::vector<std::string> descriptions = {"Sorted", "Reversed", "Random", "Partial"};
+
+    std::string sortName;
+
+    for(int i = 0; i < dataSets.size(); i++){
+        auto start = std::chrono::high_resolution_clock::now();
+        std::vector<int> dataSet = dataSets[i];
+        std::string description = descriptions[i];
+
+        switch(alg) {
+            case 1:
+                sortName = "Insertion Sort";
+                dataSet = insertionSort(dataSet);
+                break;
+            case 2:
+                sortName = "Quick Sort";
+                quickSort(dataSet, 0, dataSet.size()-1);
+                break;
+            case 3:
+                sortName = "Merge Sort";
+                dataSet = mergeSort(dataSet);
+                break;
+            case 4:
+                sortName = "Cycle Sort";
+                cycleSort(dataSet);
+                break;
+        }
+
+        std::cout << "Before " << sortName << " of " << description << " List" << std::endl;
+        print(alg, dataSet);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+
+        std::cout << "After " << sortName << " of " << description << " List" << std::endl;
+        print(alg, dataSet);
+        std::cout << "Time to sort: " << elapsed.count() << " seconds" << std::endl << std::endl;
+    }
 }
