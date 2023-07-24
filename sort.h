ce8028073b91f5f423ab4cae216f9b7878b072a9
alizarda <134419033+alizarda@users.noreply.github.com>
2023-07-24 19:38:14 -0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -19,19 +19,22 @@ class Sort {
         //Sort constructors, default and parameterized.
         Sort();
         Sort(int size, std::vector<KeyedInt> sorted, std::vector<KeyedInt> reversed, std::vector<KeyedInt> random, std::vector<KeyedInt> partial);
+
         //Menu methods.
-        void mainMenu();
-        void analyzeMenu();
-        void benchmarkMenu(int sort_type);
-        void displayMenu();
+        void mainMenu(); //The main menu pop-up.
+        void analyzeMenu(); //A menu to select the algorithm type to analyze.
+        void benchmarkMenu(int sort_type); //The menu for benchmarking (time testing, stability testing) an algorithm.
+        void displayMenu(); //The menu for displaying the steps of a sorting algorithm.
         void inputMenu(); //The user can input custom data to test against sorting algorithms.
         void fileMenu(); //The user can input a file name to select as their custom data.
         void outputFile(const std::string& file_name);
+
         //Display/analysis methods.
-        void analyzeSort(int alg, bool key);
-        void displaySort(int alg, int sort_type);
-        void print(std::vector<KeyedInt> dataSet, bool displaySteps);
+        void analyzeSort(int alg, bool key); //The method that outputs the sort analysis, either stability with key comparisons or time with the chrono c++ library.
+        void displaySort(int alg, int sort_type); //The method that calls the sorting algorithms with display steps set to true.
+        void print(std::vector<KeyedInt> dataSet, bool displaySteps); //The method that prints a dataset.
         bool checkStable(const std::vector<KeyedInt>& dataSet);
+
         //Sorting methods with their helper methods.
         std::vector<KeyedInt> insertionSort(std::vector<KeyedInt> arr, bool displaySteps = false);
         void quickSort(std::vector<KeyedInt>& arr, int minIndex, int maxIndex, bool displaySteps = false);
